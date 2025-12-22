c6c6516ff4619615268a19106fbea2181129be2c
DaMexx <haron9373@gmail.com>
2025-12-22 12:23:59 +0300

    refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,8 @@ import type { FC } from "react";
 import "./App.css";
 import router from "./router";
 import { RouterProvider } from "react-router";
-import { useAuth } from "@/features/auth";
 
 const App: FC = () => {
-  const { isAuthenticated, loading } = useAuth();
-
-  if (loading && isAuthenticated) {
-    return <div>Loading...</div>;
-  }
-
   return <RouterProvider router={router} />;
 };
 
