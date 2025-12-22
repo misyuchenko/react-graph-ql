c838ba4f18328f6976d59d9dee5903b1c1af1018
DaMexx <haron9373@gmail.com>
2025-12-22 11:44:38 +0300

    refactor

---
+++ src/app/router/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { selectIsAuthenticated } from "@/features/auth";
+import { useSelector } from "react-redux";
+
+
+const ProtectedRoute = () => {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
