df3fa77a6b6c3e792740dc7ecd8bec52db2f7722
DaMexx <haron9373@gmail.com>
2025-12-22 12:21:14 +0300

    refactor

--- src/features/auth/model/authSelectors.ts
+++ src/features/auth/model/authSelectors.ts
@@ -3,4 +3,4 @@ import type { RootState } from "@/app/store";
 export const selectAuth = (state: RootState) => state.auth;
 export const selectUser = (state: RootState) => state.auth.user;
 export const selectIsAuthenticated = (state: RootState) =>
-  state.auth.isAuthenticated;
+  state.auth.isAuthenticated && !!state.auth.user;
